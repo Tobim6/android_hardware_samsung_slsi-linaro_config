c1bd84aa67c89413db3f16f74413434df5d82b73
Your Name <your-email@example.com>
2026-09-22 17:49:49 +0200

    config: add missing config.mk for sixteen-qpr2 device-common.mk compatibility

---
+++ config.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/samsung_slsi-linaro/codec2 \
+    hardware/samsung_slsi-linaro/exynos \
+    hardware/samsung_slsi-linaro/exynos5 \
+    hardware/samsung_slsi-linaro/graphics \
+    hardware/samsung_slsi-linaro/interfaces \
+    hardware/samsung_slsi-linaro/openmax \
+    hardware/samsung_slsi-linaro/sgpu
